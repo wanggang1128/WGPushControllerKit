101738d076bd05991a7a85ef8f0104c5c0765f21
王刚 <wxwangg@163.com>
2018-09-05 11:25:51 +0800

    Initial Commit

---
+++ WGPushControllerKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGPushControllerKit
+//
+//  Created by wanggang on 2018/9/5.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGPushControllerKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGPushControllerKit
+//
+//  Created by wanggang on 2018/9/5.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
